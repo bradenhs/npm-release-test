bda484534c54f848599bf3a1e2a610598e98fbdc
Braden Snell <braden@censys.io>
2019-12-28 16:55:27 -0500

    test

--- .github/actions/pr-environment/index.js
+++ .github/actions/pr-environment/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  console.log("pr environment")
+  console.log(github.context.payload);
 } catch (error) {
   core.setFailed(error.message);
 }
