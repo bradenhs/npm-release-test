3c35b2f347c5787b2c5ea8e115ca257ca06ff906
Braden Snell <braden@censys.io>
2019-12-29 22:31:22 -0500

    test

--- .github/actions/pr-environment/index.js
+++ .github/actions/pr-environment/index.js
@@ -43,7 +43,7 @@ async function main() {
     issue_number: github.context.payload.pull_request.number,
     body: `
       **🚀 PR Environment Ready**
-      📦 Released as **\`type-route@0.0.0-${github.context.sha}\` on [NPM](https://type-route.org)/[GitHub](https://type-route.org)**
+      📦 Released as **\`type-route@0.0.0-${github.context.sha}\`** on [NPM](https://type-route.org) + [GitHub](https://type-route.org)
       🖥️ CodeSandbox playground available **[here](${prEnvironmentLink})**
     `.split("\n").map(line => line.trim()).join('\n').trim(),
     owner: "bradenhs",
